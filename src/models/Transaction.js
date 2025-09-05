dd2b40fc8e33a360106e11ed6c93db263bd35179
ashutosh229 <akumarjha875@gmail.com>
2025-09-05 16:47:26 +0530

    feat: add the transaction model

---
+++ src/models/Transaction.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema(
+  {
+    type: { type: String, enum: ["sale", "purchase"], required: true },
+    customerId: { type: mongoose.Schema.Types.ObjectId, ref: "Contact" },
+    vendorId: { type: mongoose.Schema.Types.ObjectId, ref: "Contact" },
+    products: [productItemSchema],
+    totalAmount: { type: Number, required: true },
+    date: { type: Date, default: Date.now },
+    businessId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const transactionModel = mongoose.model("Transaction", transactionSchema);
+
+export default transactionModel;
